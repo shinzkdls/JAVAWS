be4036ac9cd7eea181efe0b839a830e7280891f5
sixca <sixca0520@gmail.com>
2023-02-27 16:53:35 +0900

    0227 hyunjin a2

--- javaws/src/ws1/Ws.java
+++ javaws/src/ws1/Ws.java
@@ -1,5 +1,7 @@
 package ws1;
 
+import java.util.Arrays;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Ws {
@@ -20,10 +22,36 @@ public class Ws {
 				// 배결의 합과 평균을 출력한다.
 			} else if (cmd.equals("a2")) {
 				// 5~9까지의 숫자를 입력 받는다.
+				System.out.println("Input Number 5~9...");
+				int a2Number = sc.nextInt();
+				
 				// 입력 받은 숫자만큼 배열을 생성 하고
+				int arr[] = new int[a2Number];
+				
 				// 1~9까지의 랜덤한 숫자를 배열에 넣는다.
+				Random r = new Random();
+
+				for (int i = 0; i < arr.length; i++) {
+					arr[i] = r.nextInt(9) + 1;
+				}
+
 				// 배열 정보를 출력한다.
+				System.out.println(Arrays.toString(arr));
+				
 				// 짝수 번재 숫자들의 합과 평균을 출력한다.
+				int sum = 0;
+				double avg = 0.0;
+				int cnt = 0;
+				
+				for (int i=0; i <arr.length ; i++) {
+					if (i%2 == 0) {
+						sum += arr[i];
+						cnt++;
+					}					
+				}
+				avg = sum / (cnt*1.0);
+				System.out.printf("합계는 %d, 평균은 %4.2f\n\n", sum, avg);
+				
 			} else if (cmd.equals("a3")) {
 				// 5~9까지의 숫자를 입력 받는다.
 				// 입력 받은 숫자만큼 배열을 생성 하고
