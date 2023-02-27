ef16678e6cc614876721e3472712ccda61af0807
shinzkdls <shinzkdls@gmail.com>
2023-02-27 16:15:21 +0900

    Ws update

--- javaws/src/ws1/Ws.java
+++ javaws/src/ws1/Ws.java
@@ -13,17 +13,29 @@ public class Ws {
 				System.out.println("Quit..");
 				break;
 			} else if (cmd.equals("a1")) {
-				System.out.println("Select..");
-
+				// 5~9까지의 숫자를 입력 받는다.
+				// 입력 받은 숫자만큼 배열을 생성 하고
+				// 1~9까지의 랜덤한 숫자를 배열에 넣는다.
+				// 배열 정보를 출력한다.
+				// 배결의 합과 평균을 출력한다.
 			} else if (cmd.equals("a2")) {
-				System.out.println("Select..");
-
+				// 5~9까지의 숫자를 입력 받는다.
+				// 입력 받은 숫자만큼 배열을 생성 하고
+				// 1~9까지의 랜덤한 숫자를 배열에 넣는다.
+				// 배열 정보를 출력한다.
+				// 짝수 번재 숫자들의 합과 평균을 출력한다.
 			} else if (cmd.equals("a3")) {
-				System.out.println("Select..");
-
+				// 5~9까지의 숫자를 입력 받는다.
+				// 입력 받은 숫자만큼 배열을 생성 하고
+				// 1~9까지의 랜덤한 숫자를 배열에 넣는다.
+				// 배열 정보를 출력한다.
+				// 짝수값만의 합과 평균을 출력한다.
 			} else if (cmd.equals("a4")) {
-				System.out.println("Select..");
-
+				// 5~9까지의 숫자를 2개 입력 받는다.
+				// 2차원 배열을 만든다.
+				// 1~9까지의 랜덤한 숫자를 배열에 넣는다.
+				// 배열 정보를 출력한다.
+				// 배결의 합과 평균을 출력한다.
 			} else {
 				System.out.println("Invalid Command..");
 				System.out.println("Tri Again..");
