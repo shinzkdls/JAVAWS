2a9082e0d381f56bcc831df27b4802d57d86c118
Hayeon001 <khy8723@gmail.com>
2023-02-27 16:56:59 +0900

    0227 hayeon a1 update

--- javaws/src/ws1/Ws.java
+++ javaws/src/ws1/Ws.java
@@ -1,5 +1,7 @@
 package ws1;
 
+import java.util.Arrays;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Ws {
@@ -18,6 +20,29 @@ public class Ws {
 				// 1~9까지의 랜덤한 숫자를 배열에 넣는다.
 				// 배열 정보를 출력한다.
 				// 배결의 합과 평균을 출력한다.
+				System.out.println("5~9까지의 숫자를 입력하세요..");
+				int a = sc.nextInt();
+				
+				int arr[] = new int[a];
+				
+				Random r = new Random();
+				
+				int sum1 = 0;
+				double avg1 = 0;
+				
+				for (int i = 0; i < arr.length; i++) {
+					arr[i] = r.nextInt(9) + 1;
+					sum1 += arr[i];
+				}
+
+				System.out.println(Arrays.toString(arr));
+				
+				avg1 = sum1 / (arr.length*1.0);
+				System.out.printf("합은 %d, 평균은 %4.2f \n 입니다", sum1, avg1);
+				
+					
+			
+					
 			} else if (cmd.equals("a2")) {
 				// 5~9까지의 숫자를 입력 받는다.
 				// 입력 받은 숫자만큼 배열을 생성 하고
